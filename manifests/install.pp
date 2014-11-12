27816ffbf99431833afa251becc32a20bc9c796b
Pedro Miguel Blanco <pmblanco@prisadigital.com>
2014-11-12 19:06:10 +0100

    Aseguramos mediante la funcion ensure_resource de stdlib que el paquete python-pip esté instalado, para poder hacer la instalación de paquetes de python

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,6 @@
 class puppetboard::install {
+
+  ensure_resource('package','python-pip',{ensure => present}) 
   
   package { $puppetboard::params::puppetboard_package:
     ensure    => installed,
